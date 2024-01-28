52ea11f255185e3bc08b5f79b4e61b8a71430c41
Evelyn <130219194+EveyZeram@users.noreply.github.com>
2024-01-28 18:04:23 +0000

    Processing

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,13 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Users
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +24,34 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    # Logica para ver si el usuario existe en la DB
+    if username != "test@example.com" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+    access_token = create_access_token(identity={"username": username})
+    return jsonify(access_token=access_token)
+
+
+@api.route("/private", methods=["GET"])
+@jwt_required() # Decorador
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    # En La documentación pone que tipo de autorización debemos usar, token en este caso
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+     
+@api.route("/perfil", methods=["GET"])
+@jwt_required()
+def perfil():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    if current_user[2]:
+        print("El valor es verdadero")
+    response_body = {}
+    response_body["results"] = current_user
+    response_body["message"] = "Mostramos perfil del usuario"
+    return response_body, 200
--- src/app.py
+++ src/app.py
@@ -10,8 +10,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
-
-# from models import Person
+from flask_jwt_extended import JWTManager
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
@@ -66,6 +65,12 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
+
+
+# My code
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] =  os.getenv("JWT_SECRET")  # Change this! Semilla*
+jwt = JWTManager(app)
 
 
 # this only runs if `$ python src/main.py` is executed
